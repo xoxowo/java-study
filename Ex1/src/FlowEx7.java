d4c4667a57809f58dbb0fd15b95250e9e09bf166
xoxowo <1qq11212@gmail.com>
2022-09-20 00:25:57 +0900

    switch문 중첩문 연습-수정

--- Ex1/src/FlowEx7.java
+++ Ex1/src/FlowEx7.java
@@ -2,34 +2,37 @@ import java.util.Scanner;
 
 public class FlowEx7 {
     public static void main(String[] args){
-        System.out.print("1~4 사이의 숫자를 입력해 보세요 .>");
+        System.out.print("요일을 입력하시면 수강가능한 클래스를 안내해드립니다.\n 요일을 입력해주세요.>");
 
         Scanner scanner = new Scanner(System.in);
-        int gender = scanner.nextInt();
-        // switch 중첩 문.. 
-        switch(gender) {
-            case '1' : case '3':
-                switch (gender) {
-                    case '1' :
-                        System.out.println("1은 ");
+        String day = scanner.next();
+        // switch 중첩 문 연습 String 변수 사용
+        switch(day) {
+            case "월요일" : case "화요일":
+                switch (day) {
+                    case "월요일" :
+                        System.out.println("월요일은 마카롱 만들기 class가 있습니다.");
                         break;
-                    case '3' :
-                        System.out.println("3은");
+                    case "화요일" :
+                        System.out.println("화요일은 마들렌 만들기 class가 있습니다.");
                         break;        
                 }
                 break;    
-            case '2' : case '4':
-                switch (gender) {
-                    case '2' :
-                        System.out.println("2는");
+            case "수요일" : case "목요일": case "금요일":
+                switch (day) {
+                    case "수요일" :
+                        System.out.println("수요일은 수강 가능한 class가 없습니다.");
                         break;
-                    case '4' :
-                        System.out.println("4는 ");
+                    case "목요일" :
+                        System.out.println("목요일은 마들렌 만들기 class가 있습니다.");
                         break;  
+                    case "금요일" :
+                        System.out.println("금요일은 마카롱 만들기 class가 있습니다.");
+                        break;      
                 }    
                 break;
             default :
-                System.out.println("유효하지 않은 번호입니다.");
+                System.out.println("수강 가능한 class가 없습니다.");
         }
     }
 }
