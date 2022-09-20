9397d17508df0b38faf64d31e647f5dc1ec2d6d0
xoxowo <1qq11212@gmail.com>
2022-09-20 14:42:14 +0900

    for 문 연습 2

--- Ex1/src/FlowEx8.java
+++ Ex1/src/FlowEx8.java
@@ -22,5 +22,13 @@ public class FlowEx8 {
         for (int i=1, j=10; i <=10; i++, j--) {
             System.out.printf("%d \t %d%n", i, j);
         }
+        System.out.println("-----------------------------------------------------");
+
+        System.out.println("i \t 2*i \t 2*i-1 \t i*i \t 11-i \t i%3 \t i/3");
+        System.out.println("-----------------------------------------------------");
+
+        for (int i=1; i<=10; i++){
+            System.out.printf("%d \t %d \t %d \t %d \t %d \t %d \t %d%n", i,2*i, 2*i-1,i*i ,11-i, i%3, i/3 );
+        }
     }
 }
