24796be0bf123d82c3e9a9bd7cd65c1cb61fe21c
xoxowo <1qq11212@gmail.com>
2022-09-21 15:24:30 +0900

    향상된 for문,, not working..

--- Ex1/src/FlowEx8.java
+++ Ex1/src/FlowEx8.java
@@ -72,5 +72,24 @@ public class FlowEx8 {
             }
             System.out.println();
         }
+
+        /*향상된 for문 
+        for ( 타입 변수명 : 배열 또는 컬렉션) {
+            //반복할 문장
+        }
+        */
+        int[] arr = {1,2,3,4,5}; 
+        int summ = 0;
+
+        for (int i=0; i <arr.length; i++){
+            System.out.printf("%d ", arr[i]);
+        }
+        System.out.println();
+        // for (int tmp : arr) {
+        //     System.out.printf("%d ", tmp);
+        //     summ += tmp;
+        // }
+        System.out.println();
+        System.out.println("summ="+summ);
     }
 }
