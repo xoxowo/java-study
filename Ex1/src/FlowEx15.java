8fe4158f8f5d3ef363efe43be04d17082becf41c
xoxowo <1qq11212@gmail.com>
2022-10-03 23:13:32 +0900

    객체 배열을 만들어 인스턴스 사용

--- Ex1/src/FlowEx15.java
+++ Ex1/src/FlowEx15.java
@@ -52,3 +52,20 @@ class TvTest3 {
 
     }
 }
+
+class TvTest4 {
+    public static void main(String args[]){
+        Tv[] tvArr = new Tv[3]; // 길이가 3인 Tv객체 배열 생성 및 선언
+
+        for (int i = 0; i<tvArr.length; i++){
+            tvArr[i] = new Tv(); 
+            tvArr[i].channel = i+10; //tvArr[i]의 channel에 i+10을 저장
+        }
+        for (int i =0; i<tvArr.length; i++){
+            tvArr[i].channelUp();
+            System.out.printf("tvArr[%d].channel=%d%n",i,tvArr[i].channel);
+        }
+
+    }
+
+}
