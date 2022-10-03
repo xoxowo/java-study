c58b2d3fc30be124b241eb6c0d45b6422657e06a
xoxowo <1qq11212@gmail.com>
2022-10-03 23:09:39 +0900

    인스턴스 예제추가

--- Ex1/src/FlowEx15.java
+++ Ex1/src/FlowEx15.java
@@ -37,3 +37,18 @@ class TvTest2 {
         System.out.println("t1의 channel 값은" + t2.channel + "입니다");
     }
 }
+
+class TvTest3 {
+    public static void main(String args[]){
+        Tv t1 = new Tv();
+        Tv t2 = new Tv();
+        System.out.println("t1의 channel 값은" + t1.channel + "입니다.");
+        System.out.println("t2의 channel 값은" + t2.channel + "입니다.");
+        t2 = t1;
+        t1.channel = 8;
+        System.out.println("t1의 channel 값을 8으로 변경하였습니다."); 
+        System.out.println("t1의 channel 값은" + t1.channel + "입니다");
+        System.out.println("t1의 channel 값은" + t2.channel + "입니다");
+
+    }
+}
