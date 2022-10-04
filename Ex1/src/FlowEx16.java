ff2a0f6e8990b6c4229bbcc88a7d81008edeb39a
xoxowo <1qq11212@gmail.com>
2022-10-04 21:35:55 +0900

    변수 종류 2

--- Ex1/src/FlowEx16.java
+++ Ex1/src/FlowEx16.java
@@ -3,8 +3,8 @@ public class FlowEx16 {
 
 
 /*  선언위치에 따른 변수의 종류 
-    인스턴스 변수 
-    클래스 변수
+    인스턴스 변수 - 클래스 영역에 선언되며, 클래스의 인스턴스를 생성할 때 만들어진다. 객체생성을 해야 사용 가능
+    클래스 변수 -  class 변수는 인스턴스 변수 앞에 static만 붙이면 된다.
     지역변수 - 매서드 내에서만 사용가능 메서드가 종료되면 소멸된다.
 */
 class CardTest {
@@ -34,5 +34,5 @@ class Card {
     String kind;
     int number;
     static int width = 100;  // class 변수 객체생성을 하지 않아도 사용이 가능하다.
-    static int height = 250; // class 변수는 인스턴스 변수 앞에 static만 붙이면 된다.
+    static int height = 250; 
 }
