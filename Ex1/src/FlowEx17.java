e5897d895743689e81afef6941294efe75c2ba72
xoxowo <1qq11212@gmail.com>
2022-10-07 23:44:20 +0900

    주석 추가

--- Ex1/src/FlowEx17.java
+++ Ex1/src/FlowEx17.java
@@ -21,9 +21,9 @@ for문안에 선언된 변수가 '지역변수' (local variable)
         System.out.println("divide(5L, 3L) = " +result4);
     }
  }
-
+ // 리턴값이 없으면 void를 붙여야한다. 메서드 이름 앞에 
  class MyMath {
-    long add(long a, long b) {
+    long add(long a, long b) { // a,b 변수 (매개번수)는 여러개 들어갈 수 있지만 리턴 반환값은 1개만 출력된다.
        // long result = a+b;
        // return result;
         return a + b;  // 위 두줄을 <- 한줄로 표시할 수 있다.
