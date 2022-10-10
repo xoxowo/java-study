2b3ba05d55e531d4bbc4d361d838e25a892b0f7b
xoxowo <1qq11212@gmail.com>
2022-10-10 22:29:15 +0900

    매개변수에 따른 메서드 호출 순서와 메서드 실행 결과 값

--- Ex1/src/FlowEx20.java
+++ Ex1/src/FlowEx20.java
@@ -9,26 +9,26 @@ class Data {int x;}
 
 class ReferenceParamEx {
     public static void main(String[] args) {
-        Data d = new Data();
-        d.x = 10;
-        System.out.println("main() : x " +d.x);
+        Data d = new Data(); // 인스턴스 선언 및 생성
+        d.x = 10; //인스턴스 값 변경 
+        System.out.println("main() : x = " +d.x); //1번 실행 -> main() : x = 10
 
-        change(d);
-        System.out.println("After change(d)");
-        System.out.println("main() : x = " + d.x);
+        change(d); // change 매서드에 d값 넣어 사용 
+        System.out.println("After change(d)"); // 3번 실행
+        System.out.println("main() : x = " + d.x); // 4번 실행 -> main() : x = 1000
     }
     static void change(Data d) {
-        d.x=1000;
-        System.out.println("change() : x = " +d.x);
+        d.x=1000; // 참조형 매개변수
+        System.out.println("change() : x = " +d.x); // 2번 실행 -> change() : x = 1000
     }
 }
 
 /*  실행 결과값 
-
-main() : x 10
-change() : x = 1000
-After change(d)
-main() : x = 1000
+ 
+1번 main() : x = 10
+2번 change() : x = 1000
+3번 After change(d)
+4번 main() : x = 1000
 
  */
 
