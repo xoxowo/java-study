103bc03878a99460dbaa66248834f12823e93326
xoxowo <1qq11212@gmail.com>
2022-10-11 14:22:01 +0900

    배열길이가 1인 참조형 매개변수

--- Ex1/src/FlowEx22.java
+++ Ex1/src/FlowEx22.java
@@ -1,47 +1,25 @@
-public class FlowEx22 {
-}
+/*
+길이가 1인 메서드로 배열 호출
+ */
 
-class Data { int x ;}
-
-class ReferenceParamEx3 {
+class ReferenceParamEx2 {
     public static void main(String[] args) {
-        int[] arr = new int[] {3,2,1,6,5,4};
-        printArr(arr);
-        sortArr(arr);
-        printArr(arr);
-        System.out.println("sum="+sumArr(arr));
+        int[] x = {10};
+        System.out.println("main() : x = " +x[0]);
+
+        change(x);
+        System.out.println("After change(x)");
+        System.out.println("main() : x = " + x[0]);
+    }                   // int[] x 값이 참조형 매개변수
+    static void change(int[] x) { // 참조형 매개변수 : 읽고 값을 변경할 수 있다. 따라서 x[0]의 값이 1000으로 변경되었음.
+        x[0] = 1000;
+        System.out.println("change() : x = " + x[0]);
     }
-    static void printArr(int[] arr) {
-        System.out.print("[");
-
-        for (int i : arr) // 향상된 for 문 for (int i = 0; i <arr.length; i ++) <- 이렇게 작성해도 괜찮다.
-            System.out.print(i+",");
-        System.out.println("]");
-    }
-
-    static int sumArr(int[] arr) { // return 값이 있기 때문에 void 사용하지 않음..
-        int sum = 0;
-
-        for(int i=0;i<arr.length; i++)
-            sum +=arr[i];
-        return sum;
-    }
-
-    static void sortArr(int[] arr) {
-        for (int i=0;i<arr.length-1; i++)
-            for (int j=0;j<arr.length-1-i; j++)
-                if(arr[j] < arr[j+1]) {
-                    int tmp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = tmp;
-                }
-    } // sortArr(int[] arr)
 }
 
 /* 실행 결과값
-
-[3,2,1,6,5,4,]
-[6,5,4,3,2,1,]
-sum=21
-
+main() : x = 10
+change() : x = 1000
+After change(x)
+main() : x = 1000
  */
