2cd099883e337564c05c22bdb522ec32309e2215
xoxowo <1qq11212@gmail.com>
2022-12-20 17:35:37 +0900

    참조변수와 인스턴스의 연결2

--- Ex7/src/BindingTest.java
+++ Ex7/src/BindingTest.java
@@ -1,11 +1,11 @@
 class BindingTest {
     public static void main(String[] args) {
-        Parent p = new Child();
-        Child c = new Child();
+        Parent p = new Child(); // 인스턴스화  
+        Child c = new Child(); // 인스턴스화 
 
         System.out.println("p.x = "+ p.x);
         System.out.print("p.method() = ");
-        p.method();
+        p.method(); // 메서드는 오버라이딩 되기 때문에 child의 메서드가 호출된다.?
 
         System.out.println("c.x = "+ c.x);
         System.out.print("c.method() = ");
@@ -26,3 +26,13 @@ class Child extends Parent {
         System.out.println("Child method");
     }
 }
+
+
+/* 실행 결과
+
+p.x = 100
+p.method() = Child method
+c.x = 200
+c.method() = Child method
+
+ */
