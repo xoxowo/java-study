1deb11a2673fa0eb613136e3b0726c5e4e5bf058
xoxowo <1qq11212@gmail.com>
2022-12-20 17:59:48 +0900

    참조변수 super와 this값

--- Ex7/src/BindingTest.java
+++ Ex7/src/BindingTest.java
@@ -1,31 +1,31 @@
-// class BindingTest {
-//     public static void main(String[] args) {
-//         Parent p = new Child(); // 인스턴스화  
-//         Child c = new Child(); // 인스턴스화 
+class BindingTest {
+    public static void main(String[] args) {
+        Parent p = new Child(); // 인스턴스화  
+        Child c = new Child(); // 인스턴스화 
 
-//         System.out.println("p.x = "+ p.x);
-//         System.out.print("p.method() = ");
-//         p.method(); // 메서드는 오버라이딩 되기 때문에 child의 메서드가 호출된다.?
+        System.out.println("p.x = "+ p.x);
+        System.out.print("p.method() = ");
+        p.method(); // 메서드는 오버라이딩 되기 때문에 child의 메서드가 호출된다.?
 
-//         System.out.println("c.x = "+ c.x);
-//         System.out.print("c.method() = ");
-//         c.method();
-//     }
-// }
+        System.out.println("c.x = "+ c.x);
+        System.out.print("c.method() = ");
+        c.method();
+    }
+}
 
-// class Parent {
-//     int x = 100;
-//     void method() {
-//         System.out.println("Parent method");
-//     }
-// }
+class Parent {
+    int x = 100;
+    void method() {
+        System.out.println("Parent method");
+    }
+}
 
-// class Child extends Parent {
-//     int x = 200;
-//     void method() {
-//         System.out.println("Child method");
-//     }
-// }
+class Child extends Parent {
+    int x = 200;
+    void method() {
+        System.out.println("Child method");
+    }
+}
 
 
 /* 실행 결과
--- Ex7/src/BindingTest2.java
+++ Ex7/src/BindingTest2.java
@@ -1,4 +1,4 @@
-public class BindingTest2 {
+class BindingTest2 {
     public static void main(String[] args) {
         Parent p = new Child();
         Child c = new Child();
