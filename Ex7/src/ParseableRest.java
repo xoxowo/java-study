bd2301e6b4e57b6150eaceb87d9772e92f93ac9d
xoxowo <1qq11212@gmail.com>
2022-12-26 14:42:52 +0900

    인터페이스

--- Ex7/src/ParseableRest.java
+++ Ex7/src/ParseableRest.java
@@ -1,25 +1,27 @@
 interface Parseable {
+    // 추상 메서드 구현 public abstract 생략 
     void parse(String fileName);
 }
 
-class ParserManager {
+class ParserManager { 
+    // 리턴타입이 Parseable 인터페이스이다.
     public static Parseable getParser(String type){
+        // 매개변수 type의 값에 따라 반환타입이 다름
         if (type.equals("XML")) {
             return new XMLparser();
         } else {
             Parseable p = new HTMLparser();
             return p;
-            // return new HTMLparser();
+            // 위 두줄을 줄이면 return new HTMLparser();
         }
     }
 }
-
+// XMLparser와 HTMLparser 클래스에서 인터페이스 Parseable 구현 (메서드 내용 구현)
 class XMLparser implements Parseable {
     public void parse(String fileName) {
         System.out.println(fileName + "XML parsing completed.");
     }
 }
-
 class HTMLparser implements Parseable {
     public void parse(String fileName) {
         System.out.println(fileName + "HTML parsing completed.");
@@ -33,3 +35,8 @@ class ParseableRest {
         parser.parse("docment2.html");
     }    
 }
+
+/* 실행 결과
+docment.xmlXML parsing completed.
+docment2.htmlHTML parsing completed.
+ */
