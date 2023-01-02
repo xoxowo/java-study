9b8500fd315967d3e719f42f3da60bace6c70ced
xoxowo <1qq11212@gmail.com>
2023-01-02 13:31:36 +0900

    주석추가

--- Ex13/src/ThreadEx1.java
+++ Ex13/src/ThreadEx1.java
@@ -1,11 +1,12 @@
 class ThreadEx1 {
     public static void main(String[] args) {
-        ThreadEx1_1 t1 = new ThreadEx1_1(); // 1번 방법
+        ThreadEx1_1 t1 = new ThreadEx1_1(); // 1번 방법 상속 
         
-        Runnable r = new ThreadEx1_2(); // 2번 방법
+        Runnable r = new ThreadEx1_2(); // 2번 방법 runnable 인터페이스 구현
         Thread t2 = new Thread(r); // 생성자 Thread(Runnable target)
+        //  Thread t2 = new Thread(ThreadEx1_2()); 위 두 문장을 한줄로 
 
-        t1.start();
+        t1.start(); // 스레드는 start()로 실행할 수 있다.
         t2.start();
     }
 }
